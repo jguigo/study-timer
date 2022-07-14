e982d19a4148eb277577c5cde120520c29db91bf
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-14 01:11:04 -0300

    Create Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import Button from './components/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-        Hello World!
+      {/* funciona como se fosse uma tag html, porém é uma class */}
+        <Button />
     </div>
   )
 }
