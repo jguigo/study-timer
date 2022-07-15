19099e4d2e69cad91f3a0484a0d940e484ed9e55
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-15 02:01:49 -0300

    Add todo list on Lista component

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,29 +1,35 @@
 import React from "react";
 
 function Lista() {
+   const tarefas = [
+      {
+         tarefa: "React",
+         tempo: "02:00:00",
+      },
+      {
+         tarefa: "JavaScript",
+         tempo: "01:00:00",
+      },
+      {
+         tarefa: "TypeScript",
+         tempo: "03:00:00",
+      },
+   ];
+
    return (
       <aside>
          <h2> Estudos do dia </h2>
          <ul>
-            <li>
-               <h3>
-                  React  
-               </h3>
-               <span>
-                  02:00:00
-               </span>
-            </li>
-            <li>
-               <h3>
-                  JavaScript
-               </h3>
-               <span>
-                  01:00:00
-               </span>
-            </li>
+            {tarefas.map((item, index) => ( //esse arrow function utiliza "()"ao invés das "{}"
+               // key é uma atributo do react, para que ele não fique "perdido" quando interar uma lista
+               <li key={index}>
+                  <h3>{item.tarefa}</h3>
+                  <span>{item.tempo}</span>
+               </li>
+            ))}
          </ul>
       </aside>
-   )
+   );
 }
 
 export default Lista;
