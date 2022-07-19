00943dc7b4c2fa88645c22871ecd97838c6da55c
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-18 22:41:48 -0300

    Add timer on cronometro

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,14 +1,24 @@
 import React from "react";
-import style from './style.module.scss'
+import style from "./style.module.scss";
 
-function Relogio() {
+interface Props {
+   tempo: number | undefined;
+}
+
+function Relogio({ tempo = 0 }: Props) {
+
+   const minutos = Math.floor(tempo / 60)
+   const segundos = tempo % 60
+   const [minutosDezena, minutosUnidade] = String(minutos).padStart(2,"0")
+   const [segundoDezena, segundoUnidade] = String(segundos).padStart(2,"0")
+
    return (
       <>
-         <span className={style.relogioNumero}>0</span>
-         <span className={style.relogioNumero}>0</span>
+         <span className={style.relogioNumero}>{minutosDezena}</span>
+         <span className={style.relogioNumero}>{minutosUnidade}</span>
          <span className={style.relogioDivisao}>:</span>
-         <span className={style.relogioNumero}>0</span>
-         <span className={style.relogioNumero}>0</span>
+         <span className={style.relogioNumero}>{segundoDezena}</span>
+         <span className={style.relogioNumero}>{segundoUnidade}</span>
       </>
    );
 }
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,19 +1,31 @@
-import style from './style.module.scss'
-import Button from '../Button';
-import Relogio from './Relogio';
+import style from "./style.module.scss";
+import Button from "../Button";
+import Relogio from "./Relogio";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/Tarefas";
+import { useEffect, useState } from "react";
 
-function Cronometro(){
+interface Props{
+   selecionado: ITarefa | undefined
+}
+
+function Cronometro({selecionado}: Props) {
+   const [tempo, setTempo] = useState<number>();
+
+   useEffect(() => {
+      if(selecionado?.tempo){
+         setTempo(tempoParaSegundos(selecionado.tempo))
+      }
+   },[selecionado])
+
    return (
-      <div className={style.cronometro} >
-
+      <div className={style.cronometro}>
          <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
 
          <div className={style.relogioWrapper}>
-            <Relogio />
+            <Relogio tempo={tempo}/>
          </div>
-         <Button>
-            Começar
-         </Button>
+         <Button>Começar</Button>
       </div>
    );
 }
